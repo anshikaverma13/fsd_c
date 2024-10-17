ad70eb2be589676a2eb2abe8e624af3b4faf76af
anshikaverma13 <anshika.22b1541009@abes.ac.in>
2024-10-17 09:48:11 +0530

    commit all

---
+++ Day_5/p1.js
@@ -0,0 +1,6 @@
+// let x = '12';
+// let num = Number(x);
+// let y = '12' + '4';
+// console.log(y)
+let b = Boolean(12);
+console.log(b);
--- Day_5/script.js
+++ Day_5/script.js
@@ -1,2 +1,10 @@
-function validate()
+function validate() {
+    let uname = document.getElementById("un").value;
+    let pass= document.getElementById("pass").value;
+    if (uname == "admin" && pass == "admin") {
+        document.getElementById("res").innerHTML = "Login Success";
+    } else {
+        document.getElementById("res").innerHTML = "Login Failed";
+    }
+}   
 
